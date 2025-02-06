7b1f08419c712fa7c80290c031bc61ec4f0e00b4
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-02-06 07:00:31 +0530

    Changes

--- myproject/lib/routes.dart
+++ myproject/lib/routes.dart
@@ -7,8 +7,14 @@ import 'screens/profile_page.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => MyHomePage(),
- '/history': (context) => HistoryPage(purchaseHistory: []),
+  '/history': (context) => HistoryPage(purchaseHistory: []),
   '/trade': (context) => TradePage(),
-//  '/about': (context) => AboutPage(),
- // '/profile': (context) => ProfilePage(),
+  '/about': (context) {
+    final options = ModalRoute.of(context)?.settings.arguments as List<Map<String, dynamic>>;
+    return AboutPage(options: options);  // Pass the options to AboutPage
+  },
+  '/profile': (context) {
+    final options = ModalRoute.of(context)?.settings.arguments as List<Map<String, dynamic>>;
+    return ProfilePage(options: options);
+  }
 };
